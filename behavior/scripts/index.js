4ecfe5422de6dd3e7cacbd4c57c3d7536071685f
Kyle Byrne <kyletbyrne96@gmail.com>
2016-11-25 21:30:15 +0000

    Added custom conversation flow

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -22,6 +22,28 @@ exports.handle = (client) => {
     }
   })
 
+  const collectCity = client.createStep({
+    satisfied(){
+      return Boolean(client.getConversationState().weatherCity)
+    },
+
+    prompt(){
+      //Need to prompt user for city
+      console.log('Need to ask user for city')
+      client.done()
+    },
+  })
+
+  const provideWeather = client.createStep({
+    satisfied(){
+      return false
+    },
+    prompt() {
+      //Need to provide weather
+      client.done()
+    },
+  })
+
   const untrained = client.createStep({
     satisfied() {
       return false
@@ -41,9 +63,11 @@ exports.handle = (client) => {
       // configure responses to be automatically sent as predicted by the machine learning model
     },
     streams: {
-      main: 'onboarding',
-      onboarding: [sayHello],
-      end: [untrained],
+      main: 'getWeather',
+      hi:[sayHello],
+      getWeather: [collectCity, provideWeather],
+
+
     },
   })
 }
